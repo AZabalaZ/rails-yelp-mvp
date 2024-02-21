4644fab1151ecab3665bbe9debc3c65b208316f2
augusto <augustozab27@gmail.com>
2024-02-21 15:31:10 -0300

    rails yelp mvp

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_21_150016) do
     t.string "name"
     t.string "address"
     t.string "category"
-    t.string "phone_number"
+    t.integer "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "1111111111", category:"chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "2222222222", category:"italian"}
+zitto = {name: "zitto", address: "8 Boundary St, Paris E2 7JE", phone_number: "3333333333", category:"japanese"}
+chachingo = {name: "chachingo", address: "Peatonal", phone_number: "4444444444", category:"french"}
+club_de_la_milanesa = {name: "Club de la milanesa", address: "Maipu", phone_number: "5555555555", category:"belgian"}
+
+[dishoom, pizza_east, zitto, chachingo, club_de_la_milanesa].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
